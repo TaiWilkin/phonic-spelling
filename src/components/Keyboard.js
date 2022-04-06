7611408b760eeb22f92f8a439cb1b066d0d8c357
Tai Wilkin-Corraggio <twilkin@azavea.com>
2022-04-06 17:19:23 -0400

    Add keyboard filtering

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,40 +1,57 @@
+import { useState } from "react";
+import { Wrap, WrapItem, Tooltip, Input } from "@chakra-ui/react";
 import Key from "./Key";
-import { Wrap, WrapItem, Tooltip } from "@chakra-ui/react";
 
 const Keyboard = ({
   keys,
   handleKeyPress,
   stem,
   tooltip = "Click the keys to type.",
-}) => (
-  <Tooltip
-    label={tooltip}
-    aria-label={tooltip}
-    placement="bottom-end"
-    openDelay={1000}
-    hasArrow
-  >
-    <Wrap
-      justify="center"
-      p="5px"
-      bg="rgba(0, 0, 0, 0.1)"
-      border="1px solid rgba(0, 0, 0, 0.2)"
-      minHeight="75px"
-      m="10px"
-      spacing="10px"
+}) => {
+  const [filter, setFilter] = useState("");
+  const keySize = Math.round(75 / (keys.length / 9));
+  const filteredKeys = keys.filter((k) => k.includes(filter));
+
+  const onKeyPress = (name) => {
+    handleKeyPress(name);
+    setFilter("");
+  };
+
+  return (
+    <Tooltip
+      label={tooltip}
+      aria-label={tooltip}
+      placement="bottom-end"
+      openDelay={1000}
+      hasArrow
     >
-      {keys.map((key) => (
-        <WrapItem key={key}>
-          <Key
-            name={key}
-            onClick={handleKeyPress}
-            size={Math.round(75 / (keys.length / 9))}
-            stem={stem}
+      <Wrap
+        justify="center"
+        p="5px"
+        bg="rgba(0, 0, 0, 0.1)"
+        border="1px solid rgba(0, 0, 0, 0.2)"
+        minHeight="75px"
+        m="10px"
+        spacing="10px"
+      >
+        {keys.length > 20 && (
+          <Input
+            variant="filled"
+            bg="white"
+            placeholder="Type to filter keys"
+            size="sm"
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
           />
-        </WrapItem>
-      ))}
-    </Wrap>
-  </Tooltip>
-);
+        )}
+        {filteredKeys.map((key) => (
+          <WrapItem key={key}>
+            <Key name={key} onClick={onKeyPress} size={keySize} stem={stem} />
+          </WrapItem>
+        ))}
+      </Wrap>
+    </Tooltip>
+  );
+};
 
 export default Keyboard;
